396bda9416a589a6da919203516817e3fc0e3d8c
ziad845 <ziadfaheem681@gmail.com>
2024-12-05 20:58:37 +0200

    first Release

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import * as React from 'react';
-import { createTheme, styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
+import * as React from "react";
+import {
+  ThemeProvider,
+  createTheme,
+  styled,
+  useTheme,
+} from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
 
+import TopBar from "./components/TopBar";
+import SideBar from "./components/SideBar";
+import { getDesignTokens } from "./theme";
+import { Outlet } from "react-router-dom";
 
-import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
-
-import TopBar from './components/TopBar';
-import SideBar from './Components/SideBar';
-import { ThemeProvider } from '@emotion/react';
-import { getDesignTokens } from './theme';
-import { Outlet } from 'react-router-dom';
-
-
-const drawerWidth = 240;
-
-
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
   padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
   ...theme.mixins.toolbar,
 }));
 
-
-
 export default function MiniDrawer() {
-
   const [open, setOpen] = React.useState(false);
 
   const handleDrawerOpen = () => {
@@ -38,22 +33,29 @@ export default function MiniDrawer() {
   const handleDrawerClose = () => {
     setOpen(false);
   };
+
   const [mode, setMode] = React.useState(
-    Boolean(localStorage.getItem("currentmode") )
-      ? localStorage.getItem("currentmode") :
-      "light");
+    Boolean(localStorage.getItem("currentMode"))
+      ? localStorage.getItem("currentMode")
+      : "light"
+  );
   const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
 
   return (
     <ThemeProvider theme={theme}>
-      <Box sx={{ display: 'flex' }}>
+      <Box sx={{ display: "flex" }}>
         <CssBaseline />
-        <TopBar open={open} handleDrawerOpen={handleDrawerOpen} setMode={setMode} />
+        <TopBar
+          open={open}
+          handleDrawerOpen={handleDrawerOpen}
+          setMode={setMode}
+        />
 
         <SideBar open={open} handleDrawerClose={handleDrawerClose} />
+
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
           <DrawerHeader />
-          <Outlet/>
+          <Outlet />
         </Box>
       </Box>
     </ThemeProvider>
